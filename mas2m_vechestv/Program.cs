49c16164a3a0a91c32e6257ff87f0726302d521c
Alevtina <_@email.comgit>
2022-09-23 22:38:16 +0300

    Add massiv zapolnennii double

---
+++ mas2m_vechestv/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int m = 3;
+int n = 4;
+double[,] array = FillArray(m, n);
+PrintArray(array);
+
+double[,] FillArray(int m, int n)
+{
+   double[,] array = new double[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() * 100;
+            array[i, j] = Math.Round(array[i, j], 2);
+        }
+
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + "  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
